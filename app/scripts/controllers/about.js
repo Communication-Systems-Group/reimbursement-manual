6b8f5a68a422bf62e76012b954a9c349d8f3fc10
Robin <mail@xorox.ch>
2016-01-24 12:42:23 +0800

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reimbursementManualApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reimbursementManualApp
+ */
+angular.module('reimbursementManualApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reimbursementManualApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reimbursementManualApp
+ */
+angular.module('reimbursementManualApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
